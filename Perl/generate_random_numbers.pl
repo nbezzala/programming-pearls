d02899d84c158a2d97db4c798047e2efe559b666
Nitish Bezzala <nbezzala@yahoo.com>
2017-07-11 20:59:18 +0530

    generate random files using algorithm from chapter 12

--- Perl/generate_random_numbers.pl
+++ Perl/generate_random_numbers.pl
@@ -1,14 +1,28 @@
 #!/usr/bin/perl
+
 use Math::BigInt;
 
-my %hash;
-
-for (my $n = 1; $n <  10_000_000; $n++) {
-    $hash{$n} = 1;
-}
+my $select = 1_000_000;
+my $remaining = 10_000_000;
 
 my $file = "random.txt";
 open (my $fh, ">", $file) or die "Could not open file for write $!";
 
-print $fh join("\n", keys %hash);
+for (my $i = 0; $i < 10_000_000; $i++) {
+    if ( rand() % $remaining < $select) {
+        print $fh "$i\n";
+        $select--;
+    }
+    $remaining--;
+}
 
+
+#
+#my %hash;
+#
+#for (my $n = 1; $n <  10_000_000; $n++) {
+#    $hash{$n} = 1;
+#}
+#
+#
+#
